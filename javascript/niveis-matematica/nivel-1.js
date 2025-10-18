63c7f83263e8b3b34de9a604b146afb4483ae3ea
Felismino Chico <felisminochico35@gmail.com>
2025-10-18 19:49:16 +0100

    Continuando

--- javascript/niveis-matematica/nivel-1.js
+++ javascript/niveis-matematica/nivel-1.js
@@ -5,6 +5,8 @@ let vidasJS = 3;
 let Artigos = window.document.getElementsByTagName('article');
 const aceitarRecomecarJogo = window.document.getElementById("aceitar-recomecar");
 const avancarNivel2 = window.document.getElementById("avancar-nivel-2");
+const resultados = Array(6);
+const verificarCliques = Array(6);
 // const vetorValoresSeccao3 = [[54, 6], [48, 8], [52, 4], [42, 6], [64, 4]];
 
 //Habilitação da Secção 1
@@ -36,16 +38,16 @@ function mudancaValores(resultado, btnSeccao){
 //Mudança de Valores da Seccão 1
 valoresSeccao1[0].innerHTML = Number(Math.floor(Math.random() * ((8 - 4) + 1) + 4));
 valoresSeccao1[1].innerHTML = Number(Math.floor(Math.random() * ((8 - 4) + 1) + 4));
-let resultadoSeccao1 = valoresSeccao1[0].innerHTML * valoresSeccao1[1].innerHTML;
-mudancaValores(resultadoSeccao1, botoesSeccao1);
-botoesSeccao1[Number(Math.floor(Math.random() * botoesSeccao1.length))].innerHTML = resultadoSeccao1;
+resultados[1] = valoresSeccao1[0].innerHTML * valoresSeccao1[1].innerHTML;
+mudancaValores(resultados[1], botoesSeccao1);
+botoesSeccao1[Number(Math.floor(Math.random() * botoesSeccao1.length))].innerHTML = resultados[1];
 
 //Mudança da Secçao 2
 valoresSeccao2[0].innerHTML = Number(Math.floor(Math.random() * ((24 - 16) + 1) + 16));
 valoresSeccao2[1].innerHTML = Number(Math.floor(Math.random() * ((38 - 24) + 1) + 24));
-let resultadoSeccao2 = Number(valoresSeccao2[0].innerHTML) + Number(valoresSeccao2[1].innerHTML);
-mudancaValores(resultadoSeccao2, botoesSeccao2);
-botoesSeccao2[Number(Math.floor(Math.random() * botoesSeccao1.length))].innerHTML = resultadoSeccao2;
+resultados[2] = Number(valoresSeccao2[0].innerHTML) + Number(valoresSeccao2[1].innerHTML);
+mudancaValores(resultados[2], botoesSeccao2);
+botoesSeccao2[Number(Math.floor(Math.random() * botoesSeccao1.length))].innerHTML = resultados[2];
 
 //Mudança da Secção 3
 // let c = 0;
@@ -54,24 +56,24 @@ botoesSeccao2[Number(Math.floor(Math.random() * botoesSeccao1.length))].innerHTM
 //     c++;
 // }
 mudarValoresQuestoes(valoresSeccao3[0], valoresSeccao3[1], 90, 40, 14, 9, 5);
-let resultadoSeccao3 = Number(valoresSeccao3[0].innerHTML) / Number(valoresSeccao3[1].innerHTML);
-mudancaValores(resultadoSeccao3, botoesSeccao3);
-botoesSeccao3[Number(Math.floor(Math.random() * botoesSeccao3.length))].innerHTML = resultadoSeccao3;
+resultados[3] = Number(valoresSeccao3[0].innerHTML) / Number(valoresSeccao3[1].innerHTML);
+mudancaValores(resultados[3], botoesSeccao3);
+botoesSeccao3[Number(Math.floor(Math.random() * botoesSeccao3.length))].innerHTML = resultados[3];
 
 //Mudança da Secção 4
 valoresSeccao4[0].innerHTML = Number(Math.floor(Math.random() * ((58 - 42) + 1) + 42));
 valoresSeccao4[1].innerHTML = Number(Math.floor(Math.random() * ((26 - 15) + 1) + 15));
-let resultadoSeccao4 = Number(valoresSeccao4[0].innerHTML) - Number(valoresSeccao4[1].innerHTML);
-mudancaValores(resultadoSeccao4, botoesSeccao4);
-botoesSeccao4[Number(Math.floor(Math.random() * botoesSeccao4.length))].innerHTML = resultadoSeccao4;
+resultados[4] = Number(valoresSeccao4[0].innerHTML) - Number(valoresSeccao4[1].innerHTML);
+mudancaValores(resultados[4], botoesSeccao4);
+botoesSeccao4[Number(Math.floor(Math.random() * botoesSeccao4.length))].innerHTML = resultados[4];
 
 //Mudança da Secção 5
 valoresSeccao5[0].innerHTML = Number(Math.floor(Math.random() * ((9 - 4) + 1) + 4));
 valoresSeccao5[1].innerHTML = Number(Math.floor(Math.random() * ((8 - 3) + 1) + 3));
 valoresSeccao5[2].innerHTML = Number(Math.floor(Math.random() * ((8 - 3) + 1) + 3));
-let resultadoSeccao5 = Number(valoresSeccao5[0].innerHTML) + (Number(valoresSeccao5[1].innerHTML) * Number(valoresSeccao5[2].innerHTML));
-mudancaValores(resultadoSeccao5, botoesSeccao5);
-botoesSeccao5[Number(Math.floor(Math.random() * botoesSeccao5.length))].innerHTML = resultadoSeccao5;
+resultados[5] = Number(valoresSeccao5[0].innerHTML) + (Number(valoresSeccao5[1].innerHTML) * Number(valoresSeccao5[2].innerHTML));
+mudancaValores(resultados[5], botoesSeccao5);
+botoesSeccao5[Number(Math.floor(Math.random() * botoesSeccao5.length))].innerHTML = resultados[5];
 
 //Função para calcular Vidas
 function calcularVidas(){
@@ -91,11 +93,12 @@ function calcularPontos(){
 
 //Primeira Secão
 function primeiraSeccao(botao){
-    if(Number(botao.innerHTML) === resultadoSeccao1){
+    verificarCliques[1] = botao.textContent
+    if(botao.innerHTML === resultados[1]){
         respostaCerta(botao);
         calcularPontos();
     }else{
-        respostaErrada(botao, botoesSeccao1, resultadoSeccao1);
+        respostaErrada(botao, botoesSeccao1, resultados[1]);
         calcularVidas();
     }
     desabilitarBotoesAtuais(botoesSeccao1);
@@ -104,11 +107,12 @@ function primeiraSeccao(botao){
 
 //Segunda Secção
 function segundaSeccao(botao){
-    if(Number(botao.innerHTML) === resultadoSeccao2){
+    verificarCliques[2] = botao.textContent
+    if(botao.innerHTML === resultados[2]){
         respostaCerta(botao);
         calcularPontos();
     }else{
-        respostaErrada(botao, botoesSeccao2, resultadoSeccao2);
+        respostaErrada(botao, botoesSeccao2, resultados[2]);
         calcularVidas();
     }
     desabilitarBotoesAtuais(botoesSeccao2);
@@ -117,11 +121,12 @@ function segundaSeccao(botao){
 
 //Terceira Secção
 function terceiraSeccao(botao){
-    if(Number(botao.innerHTML) === resultadoSeccao3){
+    verificarCliques[3] = botao.textContent
+    if(botao.innerHTML === resultados[3]){
         respostaCerta(botao);
         calcularPontos();
     }else{
-        respostaErrada(botao, botoesSeccao3, resultadoSeccao3);
+        respostaErrada(botao, botoesSeccao3, resultados[3]);
         calcularVidas();
     }
     desabilitarBotoesAtuais(botoesSeccao3);
@@ -135,11 +140,12 @@ function terceiraSeccao(botao){
 
 //Quarta Secção
 function quartaSeccao(botao){
-    if(Number(botao.innerHTML) === resultadoSeccao4){
+    verificarCliques[4] = botao.textContent
+    if(botao.innerHTML === resultados[4]){
         respostaCerta(botao);
         calcularPontos();
     }else{
-        respostaErrada(botao, botoesSeccao4, resultadoSeccao4);
+        respostaErrada(botao, botoesSeccao4, resultados[4]);
         calcularVidas();
     }
     desabilitarBotoesAtuais(botoesSeccao4);
@@ -153,20 +159,35 @@ function quartaSeccao(botao){
 
 //Quinta Secção
 function quintaSeccao(botao){
-    if(Number(botao.innerHTML) === resultadoSeccao5){
+    verificarCliques[5] = botao.textContent
+    if(botao.innerHTML === resultados[5]){
         respostaCerta(botao);
         calcularPontos();
     }else{
-        respostaErrada(botao, botoesSeccao5, resultadoSeccao5);
+        respostaErrada(botao, botoesSeccao5, resultados[5]);
         calcularVidas();
     }
     desabilitarBotoesAtuais(botoesSeccao5);
 
-    if(vidasJS < 1){
-        gameOver();
-    }else{
-        proximoNivel(pontosJS, vidasJS);
-    }
+    // if(vidasJS < 1){
+    //     gameOver();
+    // }else{
+    //     proximoNivel(pontosJS, vidasJS);
+    // }
+    console.log(verificarCliques);
+    let certo = 0
+    let errado = 0
+    setTimeout(()=>{
+        for(let c = 1; c < resultados.length; c++){
+            if(verificarCliques[c] === resultados[c]){
+                certo++
+            }else{
+                errado++
+            }
+        }
+        console.log("Total de Certos: " + certo);
+        console.log("Total de Errados: " + errado);
+    }, 3500)
 }
 
 //Função para aceitar Recomeçar o jogo
@@ -176,7 +197,17 @@ aceitarRecomecarJogo.addEventListener("click", ()=>{
 
 //Função para Avançar para o Nível 2
 avancarNivel2.addEventListener("click", ()=>{
-    sessionStorage.setItem("pontos", pontosJS);
-    sessionStorage.setItem("vidas", vidasJS);
-    window.location.replace("nivel-2.html");
+    // sessionStorage.setItem("pontos", pontosJS);
+    // sessionStorage.setItem("vidas", vidasJS);
+    // window.location.replace("nivel-2.html");
 });
+
+//Colocando todos os valores de Resultados no padrão de Angola
+for(let c = 1; c < resultados.length; c++){
+    resultados[c] = Number(resultados[c]).toLocaleString("pt-AO");
+}
+
+console.log(resultados)
+console.log(verificarCliques)
+
+console.log(verificarCliques[0] === undefined)
